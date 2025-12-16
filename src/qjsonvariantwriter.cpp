3e38a6677da911afb4419ef2ca476d9adb740754
Romain-Donze <64685916+Romain-Donze@users.noreply.github.com>
2025-12-16 12:47:45 +0100

    Handle QDateTime serialization in JSON writer

--- src/qjsonvariantwriter.cpp
+++ src/qjsonvariantwriter.cpp
@@ -122,9 +122,13 @@ static inline void variantValueToJson(const QVariant &value, QIODevice *d)
             d->write("null"); // +INF || -INF || NaN (see RFC4627#section2.4)
         break;
     }
+    case QMetaType::QDateTime: {
+        const QDateTime val = value.toDateTime().toUTC();
+        stringToJson(val.toString(Qt::ISODate), d);
+        break;
+    }
     case QMetaType::Nullptr:
     case QMetaType::QString:
-    case QMetaType::QDateTime:
     default:
         if(value.isNull() || !value.isValid()) {
             d->write("null");
